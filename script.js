7247d251a70eb3034aebc4ed87135888081b6cfd
LucasSalda <lucassalda7485@gmail.com>
2020-01-21 00:48:34 -0300

    fin 21/01/20.

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("La imagen de aorus hay que cambiarla");
+function sumar() {
+    var numeros= document.getElementById("testo").value;
+    numeros++;
+    alert(numeros);
+}
+
+function restar() {
+    var numeros= document.getElementById("testo").value;
+    numeros--;
+    alert(numeros);
+}
